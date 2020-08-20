b613e492936263dcf5bd78cf40f6d63819db1828
Diyari <65598554+Diyari-Kurdi@users.noreply.github.com>
2020-08-20 22:45:32 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,15 +1,16 @@
 using System;
-using System.Windows.Forms;
+using System.Data;
 
-namespace WindowsFormsApp1
+namespace NumberToKurdishWord
 {
-    public partial class Form1 : Form
+    public class NumToWord
     {
-        public Form1()
+
+        public string GetWords(Int64 number)
         {
-            InitializeComponent();
+            return NumberToWords(number);
         }
-        
+
         int[] thousends =
             {
             1000,
@@ -70,125 +71,182 @@ namespace WindowsFormsApp1
              800000000,
              900000000
         };
+        Int64[] billions =
+            {
+            1000000000,
+            2000000000,
+            3000000000,
+            4000000000,
+            5000000000,
+            6000000000,
+            7000000000,
+            8000000000,
+            9000000000,
+            10000000000,
+             20000000000,
+             30000000000,
+             40000000000,
+             50000000000,
+             60000000000,
+             70000000000,
+             80000000000,
+             90000000000,
+             100000000000,
+             200000000000,
+             300000000000,
+             400000000000,
+             500000000000,
+             600000000000,
+             700000000000,
+             800000000000,
+             900000000000
+        };
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private string NumberToWords(Int64 number)
         {
-            try
-            {
-                textBox2.Text = NumberToWords(Convert.ToInt32(textBox1.Text))+" دۆلار ";
-            }
-            catch { }
-        }
-
-        private string NumberToWords(int number)
-        {
-        
-        bool containsthousends = false;
-
-            foreach (int n in thousends)
-            {
-                if (n == number) 
-                {
-                    containsthousends = true;
-                    break;
-                }
-            }
-            bool containsMillions = false;
-
-            foreach (int n in millions)
-            {
-                if (n == number)
-                {
-                    containsMillions = true;
-                    break;
-                }
-            }
-        
-            if (number == 0)
-                return "سفر";
-
-            if (number < 0)
-                return "کەم " + NumberToWords(Math.Abs(number));
-
             string words = "";
+            if (number <= 999999999999)
+            {
+                bool containsthousends = false;
 
-            if ((number / 1000000) > 0)
-            {
-                if (number > 1009999 && number< 999999999 && containsMillions!=true) 
+                foreach (int n in thousends)
                 {
-                    words += NumberToWords(number / 1000000) + " ملیۆن و ";
-                    number %= 1000000;
-                }
-                else 
-                {
-                    words += NumberToWords(number / 1000000) + " ملیۆن ";
-                    number %= 1000000;
-                }
-                
-            }
-
-            if ((number / 1000) > 0)
-            {
-                if (number > 1099 && number < 9999999 && containsthousends != true)
-                {
-                    words += NumberToWords(number / 1000) + " هەزار و ";
-                    number %= 1000;
-                }
-                else
-                {
-                    words += NumberToWords(number / 1000) + " هەزار ";
-                    number %= 1000;
-                }
-            }
-            if (number != 100)
-            {
-                if (number > 199)
-                {
-                    if ((number / 100) > 0)
+                    if (n == number)
                     {
-                        words += NumberToWords(number / 100) + " سەد ";
+                        containsthousends = true;
+                        break;
+                    }
+                }
+                bool containsMillions = false;
+
+                foreach (int n in millions)
+                {
+                    if (n == number)
+                    {
+                        containsMillions = true;
+                        break;
+                    }
+                }
+
+                bool containsBillions = false;
+
+                foreach (Int64 n in billions)
+                {
+                    if (n == number)
+                    {
+                        containsBillions = true;
+                        break;
+                    }
+                }
+
+                if (number == 0)
+                    return "سفر";
+
+                if (number < 0)
+                    return "کەم " + NumberToWords(Math.Abs(number));
+
+
+
+                if ((number / 1000000000) > 0)
+                {
+
+                    if (number > 1000099999 && number < 9999999999 && containsBillions != true)
+                    {
+                        words += NumberToWords(number / 1000000000) + " بلیۆن و ";
+                        number %= 1000000000;
+                    }
+                    else
+                    {
+                        words += NumberToWords(number / 1000000000) + " بلیۆن ";
+                        number %= 1000000000;
+                    }
+
+                }
+
+                if ((number / 1000000) > 0)
+                {
+                    if (number > 1009999 && number < 999999999 && containsMillions != true)
+                    {
+                        words += NumberToWords(number / 1000000) + " ملیۆن و ";
+                        number %= 1000000;
+                    }
+                    else
+                    {
+                        words += NumberToWords(number / 1000000) + " ملیۆن ";
+                        number %= 1000000;
+                    }
+
+                }
+
+
+                if ((number / 1000) > 0)
+                {
+                    if (number > 1099 && number < 9999999 && containsthousends != true)
+                    {
+                        words += NumberToWords(number / 1000) + " هەزار و ";
+                        number %= 1000;
+                    }
+                    else
+                    {
+                        words += NumberToWords(number / 1000) + " هەزار ";
+                        number %= 1000;
+                    }
+
+
+                }
+                if (number != 100)
+                {
+                    if (number > 199)
+                    {
+                        if ((number / 100) > 0)
+                        {
+                            words += NumberToWords(number / 100) + " سەد ";
+                            number %= 100;
+                        }
+                    }
+                    else if (!(number <= 100))
+                    {
+                        words += " سەد ";
                         number %= 100;
                     }
                 }
-                else if(!(number<=100))
-                {
-                    words +=  " سەد ";
-                    number %= 100;
-                }
-            }
-            else 
-            {
-                words +=" سەد ";
-                number %= 100;
-            }
-
-
-            if (number > 0)
-            {
-                if (words != "")
-                    words += " و ";
-
-                var units = new[] 
-                {
-                    "سفر", "یەک", "دوو", "سێ", "چوار", "پێنچ", "شەش", "حەوت", "هەشت", "نۆ", "دە", "یازدە", "دوازدە", "سێزدە", "چواردە", "پازدە", "شازدە", "حەڤدە", "هەژدە", "نۆزدە" 
-                };
-                var tens = new[] 
-                {
-                    "سفر", "دە", "بیست", "سی", "چل", "پەنجا", "شێست", "حەفتا", "هەشتا", "نەوەت" 
-                };
-
-                if (number < 20)
-                    words += units[number];
                 else
                 {
-                    words += tens[number / 10];
-                    if ((number % 10) > 0)
-                        words += " و " + units[number % 10];
+                    words += " سەد ";
+                    number %= 100;
+                }
+
+
+                if (number > 0)
+                {
+                    if (words != "")
+                        words += " و ";
+
+                    var units = new[]
+                    {
+                    "سفر", "یەک", "دوو", "سێ", "چوار", "پێنچ", "شەش", "حەوت", "هەشت", "نۆ", "دە", "یازدە", "دوازدە", "سێزدە", "چواردە", "پازدە", "شازدە", "حەڤدە", "هەژدە", "نۆزدە"
+                };
+                    var tens = new[]
+                    {
+                    "سفر", "دە", "بیست", "سی", "چل", "پەنجا", "شێست", "حەفتا", "هەشتا", "نەوەت"
+                };
+
+                    if (number < 20)
+                        words += units[number];
+                    else
+                    {
+                        words += tens[number / 10];
+                        if ((number % 10) > 0)
+                            words += " و " + units[number % 10];
+                    }
                 }
             }
+            else
+            {
+                return "ئەم ژمارەیە پشتگیری لێ ناکرێت!";
+            }
             return words;
+
         }
-
-
     }
 }
